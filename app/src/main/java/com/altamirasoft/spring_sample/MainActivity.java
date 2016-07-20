09d43e2b8df4a1a923320b1e2ed7e069450d3eec
bdhwan <bdhwan@gmail.com>
2016-07-20 16:53:45 +0900

    add preview and jar

--- app/src/main/java/com/altamirasoft/spring_sample/MainActivity.java
+++ app/src/main/java/com/altamirasoft/spring_sample/MainActivity.java
@@ -2,9 +2,12 @@ package com.altamirasoft.spring_sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.altamirasoft.spring.SpringAnimator;
+import com.altamirasoft.spring.SpringAnimatorSet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,42 +21,63 @@ public class MainActivity extends AppCompatActivity {
 
         object = findViewById(R.id.object);
 
-        object.setOnClickListener(new View.OnClickListener() {
+
+        object.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int action = motionEvent.getActionMasked();
+                if(action==MotionEvent.ACTION_DOWN){
+                    Log.d("log","value = ACTION_DOWN");
+                    SpringAnimator.ofFloat(object,"scale",1f,0.5f).setTensionFriction(400,20).start();
 
-                final SpringAnimator anim =  SpringAnimator.ofFloat(object,"translationY",0,200);
-                anim.addUpdateListener(new SpringAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(SpringAnimator animation) {
-                        float value = animation.getAnimatedValue();
-                        object.setTranslationX(value);
-                    }
-                });
+                    return true;
 
-                anim.addListener(new SpringAnimator.AnimatorListener() {
-                    @Override
-                    public void onAnimationStart(SpringAnimator animation) {
+                }
+                else if(action==MotionEvent.ACTION_UP){
+                    Log.d("log","value = ACTION_UP");
+                    //with animator set
+                    SpringAnimator animX = SpringAnimator.ofFloat(object,"scaleX",1f);
+                    SpringAnimator animY = SpringAnimator.ofFloat(object,"scaleY",1f);
 
-                    }
 
-                    @Override
-                    public void onAnimationEnd(SpringAnimator animation) {
+                    animX.addUpdateListener(new SpringAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(SpringAnimator animation) {
+                            float value = animation.getAnimatedValue();
+                            Log.d("log","value = "+value);
 
-                    }
 
-                    @Override
-                    public void onAnimationCancel(SpringAnimator animation) {
+                        }
+                    });
 
-                    }
+                    animX.addListener(new SpringAnimator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(SpringAnimator animation) {
 
-                    @Override
-                    public void onAnimationRepeat(SpringAnimator animation) {
+                        }
 
-                    }
-                });
-                anim.start();
+                        @Override
+                        public void onAnimationEnd(SpringAnimator animation) {
 
+                        }
+
+                        @Override
+                        public void onAnimationCancel(SpringAnimator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(SpringAnimator animation) {
+
+                        }
+                    });
+
+                    SpringAnimatorSet set = SpringAnimatorSet.createAnimationSet(animX,animY);
+                    set.start();
+                    return true;
+                }
+
+                return false;
             }
         });
 
